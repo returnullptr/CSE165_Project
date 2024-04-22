fda29f5305cf04067ab1e4e117ba202b5687ee09
Rocky Ren <rren21@icloud.com>
2024-04-22 01:57:38 -0700

    Added Json

--- main.cpp
+++ main.cpp
@@ -7,9 +7,27 @@
 
 void writeJson() {
     QJsonObject rootObj;
+
+    // Country name
+    rootObj.insert("name", "United States");
+
+    // Country info
+    QJsonObject infoObj;
+    infoObj.insert("Capital", "Washington D.C.");
+    infoObj.insert("Asia", true);
+    infoObj.insert("Founded Year", 1776);
+
+    rootObj.insert("info", infoObj);
+
+    // Country states
+    QJsonArray stateArray;
+
+    QJsonObject stateCAObj;
+    stateCAObj.insert("name", "California");
+    stateCAObj.insert("capital", "Sacramento");
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
     writeJson();
